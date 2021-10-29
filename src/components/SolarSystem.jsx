4c5d201d7a67e2891f7489471089fdfd3e4ec1b4
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-29 15:17:05 -0300

    feat: Feita implementação do componente Title (Requisito3)

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system">
-        text
-      </div>
+      <div data-testid="solar-system"> </div>
     );
   }
 }
