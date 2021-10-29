fe227404f3151b4264e0ac757ad6e7eee34b6d26
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-29 15:23:15 -0300

    feat: Feita implementação do componente Title dentro do componente SolarSystem (Requisito4)

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Title from './Title';
+// import PropTypes from 'prop-types';
 
 class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system"> </div>
+      <div data-testid="solar-system">
+        <Title headline="planetas" />
+      </div>
     );
   }
 }
+
+// SolarSystem.propTypes = {
+// };
 
 export default SolarSystem;
