62b1d36a6b3a5b380881b18c2450b1248a3e4df6
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-29 16:25:40 -0300

    feat: Feita implementação do PlanetCard no componente SolarSystem (Requisito6)

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -7,7 +7,7 @@ class PlanetCard extends Component {
 
     return (
       <div data-testid="planet-card">
-        <p>{planetName}</p>
+        <p data-testid="planet-name">{planetName}</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 // import PropTypes from 'prop-types';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline="planetas" />
+        <Title headline="Planetas" />
+        { planets.map((planet) => (
+          <PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />
+        ))}
       </div>
     );
   }
