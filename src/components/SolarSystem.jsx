43b89f4d399a54ef6cd89e17307498f0b2302e07
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-29 15:04:29 -0300

    feat: Feita implementação do componente SolarSystem (Requisito2)

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        text
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
